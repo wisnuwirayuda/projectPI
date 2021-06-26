dbb03d84b8213b0d92795926ba47d189c6e2bcc6
wisnu wirayuda <wisnu.wirayuda01@gmail.com>
2021-06-27 01:33:18 +0700

    Instalasi Third Party (React Navigation)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import {Splash} from './pages';
 import {GetStarted} from './pages';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
-  return <GetStarted></GetStarted>;
+  return (
+    <NavigationContainer>
+      <GetStarted></GetStarted>
+    </NavigationContainer>
+  );
 };
 
 export default App;
