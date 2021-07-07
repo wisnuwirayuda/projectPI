48a58b60a443dbf6f97cdd6afe26faebf70cdf70
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2021-07-07 21:24:55 +0700

    Menginstall Library & Setup Konfigurasi Firebase

---
+++ src/config/Firebase/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBVRmRYYXRY2-R7-Y9LsCckczDQJFT6m-Y',
+  authDomain: 'my-doctor-63e76.firebaseapp.com',
+  projectId: 'my-doctor-63e76',
+  storageBucket: 'my-doctor-63e76.appspot.com',
+  messagingSenderId: '853956959809',
+  appId: '1:853956959809:web:20920c45ba1754acddf0c0',
+});
+
+const Firebase = firebase;
+
+export default Firebase;
