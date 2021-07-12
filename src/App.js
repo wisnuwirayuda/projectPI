8a3f7522ff7070292e14bdf0103b069e5b2cbf71
Wisnu Trenggono Wirayuda <wisnu.wirayuda01@gmail.com>
2021-07-12 23:38:01 +0700

    Menginstall & Setup Library Redux, dan React Redux Untuk State Management

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import Router from './router';
 import FlashMessage from 'react-native-flash-message';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <NavigationContainer>
         <Router></Router>
       </NavigationContainer>
       <FlashMessage position="top"></FlashMessage>
-    </>
+    </Provider>
   );
 };
 
---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import {createStore} from 'redux';
+
+const initialState = {
+  loading: false,
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+const store = createStore();
+
+export default store;
